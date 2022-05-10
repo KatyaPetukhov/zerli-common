8672b480377b80f9517e346fe231ef48266bae53
Katya Petukhov <omeciano@gmail.com>
2022-05-10 19:58:18 +0300

    Added type PING

--- src/common/RequestType.java
+++ src/common/RequestType.java
@@ -2,8 +2,9 @@ package common;
 
 public enum RequestType {
 	// List of requests supported by server
-	UNKNOWN_REQUEST,  // data is null - Something went wrong on server side
+	REQUEST_FAILED,  // data is null - Something went wrong on server side
 	FORBIDDEN,  // data is null - User is not authorized
+	PING,  // Checks is server is alive. Should return unmodified request.
 	GET_USER,  // data of class User
 	GET_ITEMS,
 	SUBMIT_ORDER,
