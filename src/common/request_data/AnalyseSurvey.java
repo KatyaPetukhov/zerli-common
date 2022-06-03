a600e5aa66b3fbc6a2c05fa74ab5161f906c7a30
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2022-06-03 13:15:46 +0300

    description

--- src/common/request_data/AnalyseSurvey.java
+++ src/common/request_data/AnalyseSurvey.java
@@ -2,6 +2,11 @@ package common.request_data;
 
 import common.JSONObject;
 
+/**
+   *this is an json class that represents the survey analayze 
+   *by this class we can transfer the survey analyze data between user->client->sql
+  *@author Jessica and Yarden
+   */
 public class AnalyseSurvey extends JSONObject {
 
 	public String syrveyType;
@@ -15,6 +20,10 @@ public class AnalyseSurvey extends JSONObject {
 		
 	}	
 	
+/**
+   *A constructor who maintains the survey values to be analyzed
+  *@author Jessica and Yarden
+   */
 	public AnalyseSurvey(String syrveyType, String shopName, String date) {
 		this.syrveyType = syrveyType;
 		this.shopName = shopName;
