3e65133a016634ebe2784770b76681160f639028
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2022-06-03 13:20:46 +0300

    description

--- src/common/request_data/CategoriesList.java
+++ src/common/request_data/CategoriesList.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 import common.JSONObject;
 
+/**
+   *this is an json class that represents the Catagories list in the menu
+   *by this class we can transfer the category list  data between user->client->sql
+  *@author Katya and Ahron
+   */
 public class CategoriesList extends JSONObject {
 	public ArrayList<String> items; // Not List because it should be initialized by default during JSON creation.
 	
