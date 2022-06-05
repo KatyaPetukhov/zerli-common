4dfe85b60cdcb1d976685e2f4f81f89770b0abbb
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2022-06-05 23:42:17 +0300

    description

--- src/common/request_data/ProductList.java
+++ src/common/request_data/ProductList.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 import common.JSONObject;
 
+/**
+   *JSON class that saves products in store in a list , and can tranfer it between client and server 
+   *
+  *@author Katya
+   */
+
 public class ProductList extends JSONObject {
 	public String category;
 	public ArrayList<Product> items; // Not List because it should be initialized by default during JSON creation.
