17688adc199824386cd70c227604083c38ff5f24
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2022-06-06 00:07:34 +0300

    description

--- src/common/interfaces/CartManager.java
+++ src/common/interfaces/CartManager.java
@@ -1,6 +1,10 @@
 package common.interfaces;
 
-
+/**
+   *An interface implemented on both the client and the server with the same functions,
+   *this interface has the functionality related to the user cart -add to cart,update cart,finish an order...
+  *@Katya
+   */
 import common.request_data.Order;
 
 public interface CartManager {
