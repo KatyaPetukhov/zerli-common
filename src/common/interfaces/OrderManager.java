d9e434e870d3581923da4f19884ae3bb6fae58c0
jessicamirov <99083980+jessicamirov@users.noreply.github.com>
2022-06-06 00:06:21 +0300

    Update OrderManager.java

--- src/common/interfaces/OrderManager.java
+++ src/common/interfaces/OrderManager.java
@@ -2,7 +2,11 @@ package common.interfaces;
 
 import common.request_data.Order;
 import common.request_data.OrderList;
-
+/**
+   *An interface implemented on both the client and the server with the same functions,
+   *this interface has the functionality related to orders -get user orders, add orders and change order.
+  *@Katya
+   */
 public interface OrderManager {
 	public Order getOrder(String username);
 	public OrderList getOrders(String username);
